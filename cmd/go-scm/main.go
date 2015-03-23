c32e60358b1d51904f87e084bf5291d6143aebae
Peter Edge <peter.edge@gmail.com>
2015-03-23 20:00:37 +0100

    remove etcd

--- cmd/go-scm/main.go
+++ cmd/go-scm/main.go
@@ -10,8 +10,6 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/coreos/go-etcd/etcd"
-	"github.com/peter-edge/go-etcdmarshal"
 	"github.com/peter-edge/go-exec"
 	"github.com/peter-edge/go-scm"
 )
@@ -22,40 +20,22 @@ func main() {
 	var clonePath string
 	var tarballName string
 	var ignoreCheckoutFiles bool
-	var etcdUrl string
-	var etcdInputKey string
-	var etcdOutputKey string
 	flag.StringVar(&baseDirPath, "base_dir_path", "", "The directory to clone into (defaults to a temporary directory)")
 	flag.StringVar(&hostBaseDirPath, "host_base_dir_path", "", "The equivalent directory within the host if base_dir_path is a linked volume (base_dir_path must be set)")
 	flag.StringVar(&clonePath, "clone_path", "", "The name of the clone directory (defaults to clone)")
 	flag.StringVar(&tarballName, "tarball_name", "", "The name of the tarball to output (no tarball by default)")
 	flag.BoolVar(&ignoreCheckoutFiles, "ignore_checkout_files", false, "Ignore checkout files if tarballing (false by default)")
-	flag.StringVar(&etcdUrl, "etcd_url", "", "The etcd url")
-	flag.StringVar(&etcdInputKey, "etcd_input_key", "", "The etcd input key")
-	flag.StringVar(&etcdOutputKey, "etcd_output_key", "", "The etcd output key")
 	flag.Parse()
 	checkTrue(!(clonePath != "" && tarballName != ""), "Cannot have both --clone_path and --tarball_name set")
 	if hostBaseDirPath != "" {
 		checkTrue(baseDirPath != "", "--base_dir_path must be set if --host_base_dir_path is set")
 	}
 	checkTrue(!(tarballName == "" && ignoreCheckoutFiles), "Cannot set --ignoreCheckoutFiles if --tarball_name is not set")
-	checkTrue(((etcdUrl == "") == (etcdInputKey == "")) && ((etcdInputKey == "") == (etcdOutputKey == "")), "All of --etcd_url, --etcd_input_key, --etcd_output_key must be set or not set")
 
+	data, err := ioutil.ReadAll(os.Stdin)
+	checkError(err)
 	var externalCheckoutOptions scm.ExternalCheckoutOptions
-	if etcdUrl == "" {
-		data, err := ioutil.ReadAll(os.Stdin)
-		checkError(err)
-		checkError(json.Unmarshal(data, &externalCheckoutOptions))
-	} else {
-		etcdmarshalApi := etcdmarshal.NewJsonApi(
-			etcd.NewClient(
-				[]string{
-					etcdUrl,
-				},
-			),
-		)
-		checkError(etcdmarshalApi.Read(etcdInputKey, &externalCheckoutOptions))
-	}
+	checkError(json.Unmarshal(data, &externalCheckoutOptions))
 	checkoutOptions, err := scm.ConvertExternalCheckoutOptions(&externalCheckoutOptions)
 	checkError(err)
 
@@ -92,18 +72,7 @@ func main() {
 	if hostBaseDirPath != "" {
 		path = strings.NewReplacer(baseDirPath, hostBaseDirPath).Replace(path)
 	}
-	if etcdUrl == "" {
-		fmt.Println(path)
-	} else {
-		etcdmarshalApi := etcdmarshal.NewStringApi(
-			etcd.NewClient(
-				[]string{
-					etcdUrl,
-				},
-			),
-		)
-		checkError(etcdmarshalApi.Write(etcdOutputKey, path))
-	}
+	fmt.Println(path)
 	os.Exit(0)
 }
 
