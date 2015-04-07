52d389810e492a39a5ca3cd381550358e5cb0907
Peter Edge <peter.edge@gmail.com>
2015-04-07 19:20:26 +0200

    commit

--- makebin/compile.sh
+++ makebin/compile.sh
@@ -17,9 +17,9 @@ esac
 rm -rf "/tmp/goscm_build/${GOOS}"
 mkdir -p "/tmp/goscm_build/${GOOS}"
 CGO_ENABLED=0 GOOS=${GOOS} GOARCH=amd64 go build -a -compiler gc -installsuffix cgo -ldflags '-d -s -w' -o "/tmp/goscm_build/${GOOS}/scm-clone" cmd/scm-clone/main.go
-CGO_ENABLED=0 GOOS=${GOOS} GOARCH=amd64 go build -a -compiler gc -installsuffix cgo -ldflags '-d -s -w' -o "/tmp/goscm_build/${GOOS}/scm-checkout" cmd/scm-checkout/main.go
+CGO_ENABLED=0 GOOS=${GOOS} GOARCH=amd64 go build -a -compiler gc -installsuffix cgo -ldflags '-d -s -w' -o "/tmp/goscm_build/${GOOS}/scm-tarball" cmd/scm-tarball/main.go
 mkdir -p "tmp/${GOOS}"
 cp "/tmp/goscm_build/${GOOS}/scm-clone" "tmp/${GOOS}/scm-clone"
-cp "/tmp/goscm_build/${GOOS}/scm-checkout" "tmp/${GOOS}/scm-checkout"
+cp "/tmp/goscm_build/${GOOS}/scm-tarball" "tmp/${GOOS}/scm-tarball"
 echo "tmp/${GOOS}"
 ls -lh "tmp/${GOOS}"
